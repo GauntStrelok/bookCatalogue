d79d534955dc0d1505c87d1ae2b261962b071e02
Pablo Gotelli <pablonahuelgotelli@gmail.com>
2020-05-31 21:49:19 -0300

    First commit book catalogue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
+import Book from './components/Book';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div className="App">
+    <div className="BooksContainer">
+      <Book src="https://i.imgur.com/vl7NdiI.jpg" title="Star Wars"></Book>
+      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp" title="Aramburu"></Book>
+      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp" title="Algo de Lyna"></Book>
+      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp" title="Amores"></Book>
+      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp" title="Esto es soloo una prueba"></Book>
+      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp" title="Ignorar titulos"></Book>
+      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp" title="Estos son los libros"></Book>
+      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp" title="Hace click"></Book>
     </div>
-  );
+    <div className="BooksContainer">
+      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
+      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
+    </div>
+    <div className="BooksContainer">
+      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
+      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
+    </div>
+    <div className="BooksContainer">
+      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
+      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
+    </div>
+    <div className="BooksContainer">
+      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
+      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
+      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
+    </div>
+  </div>);
 }
 
 export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./Book.css";
+import Modal from "react-modal";
+
+export default function Book(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  return (
+    <div>
+      <div className="imageContainer" onClick={openModal}>
+        <img src={props.src} className="image" />
+        <div className="title">{props.title || "Titulo de Libro"}</div>
+      </div>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={closeModal}
+        contentLabel="Minimal Modal Example"
+      >
+        <img src={props.src} className="image2" />
+        <div>{props.title || "Titulo de Libro"}</div>
+        <div>
+          {" "}
+          Ipsum is simply dummy text of the printing and typesetting industry.{" "}
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum
+        </div>
+      </Modal>
+    </div>
+  );
+}
