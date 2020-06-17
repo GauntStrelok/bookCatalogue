86fd8ad0547e1c7447a58ce9adca9b1ed1e5887b
Pablo Gotelli <pablonahuelgotelli@gmail.com>
2020-06-16 23:26:43 -0300

    agregado template base

--- src/App.js
+++ src/App.js
@@ -2,66 +2,87 @@ import React, {useState, useEffect} from 'react';
 import Book from './components/Book';
 import firebaseAuth from './authentication/firebaseAuth'
 import './App.css';
+import './css/bootstrap.min.css';
+import './css/fontawesome-all.min.css';
+import './css/tooplate-style.css';
+import $ from 'jquery';
 
 function App() {
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-
+    $(function() {
+      $('body').addClass('loaded');
+      //$('.tm-current-year').text(new Date().getFullYear());  Update year in copyright
+    });
     //firebaseAuth();
 
   });
 
-  return (<div className="App">
-    <div className="BooksContainer">
-      <Book src="https://i.imgur.com/vl7NdiI.jpg" title="Star Wars"></Book>
-      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp" title="Aramburu"></Book>
-      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp" title="Algo de Lyna"></Book>
-      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp" title="Amores"></Book>
-      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp" title="Esto es soloo una prueba"></Book>
-      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp" title="Ignorar titulos"></Book>
-      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp" title="Estos son los libros"></Book>
-      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp" title="Hace click"></Book>
+  return (<div>
+    <div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
     </div>
-    <div className="BooksContainer">
-      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
-      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
-    </div>
-    <div className="BooksContainer">
-      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
-      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
-    </div>
-    <div className="BooksContainer">
-      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
-      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
-    </div>
-    <div className="BooksContainer">
-      <Book src="https://i.imgur.com/vl7NdiI.jpg"></Book>
-      <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/ser-la-mejor-version-libro-gabriel-busso-D_NQ_NP_765485-MLA32255546797_092019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/una-familia-anormal-3-lyna-vallejos-libro-nuevo-altea-D_NQ_NP_901981-MLA40583947416_012020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/el-alma-de-las-flores-viviana-rivero-libro-nuevo-planeta-D_NQ_NP_639961-MLA33073881273_122019-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-mas-luz-x-favor-connie-isla-tenes-el-poder-de-cambiar-D_NQ_NP_862666-MLA41886319733_052020-V.webp"></Book>
-      <Book src="https://http2.mlstatic.com/libro-besame-mucho-pediatra-carlos-gonzalez-local-papel-D_NQ_NP_13590-MLA3035353267_082012-V.webp"></Book>
+
+    <div class="tm-main">
+
+      <div class="tm-welcome-section">
+        <div class="container tm-navbar-container">
+          <div class="row">
+            <div class="col-xl-12">
+              <nav class="navbar navbar-expand-sm">
+                <ul class="navbar-nav ml-auto"></ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <div class="container text-center tm-welcome-container">
+          <div class="tm-welcome">
+            <h1 class="text-uppercase mb-3 tm-site-name">Akira Books</h1>
+            <p class="tm-site-description">Busqueda Libros mercado libre</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="container">
+        <div class="tm-search-form-container">
+          <form action="index.html" method="GET" class="form-inline tm-search-form">
+            <div class="text-uppercase tm-new-release">BUSQUEDA</div>
+            <div class="form-group tm-search-box">
+              <input type="text" name="keyword" class="form-control tm-search-input" placeholder="Type your keyword ..."/>
+              <input type="submit" value="Search" class="form-control tm-search-submit"></input>
+            </div>
+
+          </form>
+        </div>
+
+        <div class="row tm-albums-container grid">
+          <div class="col-sm-6 col-12 col-md-4 col-lg-2 col-xl-2 tm-album-col">
+            <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp" title="Algo de Lyna"></Book>
+          </div>
+          <div class="col-sm-6 col-12 col-md-4 col-lg-2 col-xl-2 tm-album-col">
+            <Book src="https://i.imgur.com/vl7NdiI.jpg" title="Star Wars"></Book>
+          </div>
+          <div class="col-sm-6 col-12 col-md-4 col-lg-2 col-xl-2 tm-album-col">
+            <Book src="https://http2.mlstatic.com/libro-basta-de-amores-de-mierda-D_NQ_NP_742437-MLA31068254336_062019-V.webp" title="Aramburu"></Book>
+          </div>
+          <div class="col-sm-6 col-12 col-md-4 col-lg-2 col-xl-2 tm-album-col">
+            <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp" title="Algo de Lyna"></Book>
+          </div>
+          <div class="col-sm-6 col-12 col-md-4 col-lg-2 col-xl-2 tm-album-col">
+            <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp" title="Algo de Lyna"></Book>
+          </div>
+          <div class="col-sm-6 col-12 col-md-4 col-lg-2 col-xl-2 tm-album-col">
+            <Book src="https://http2.mlstatic.com/aramburu-maria-o-donnell-libro-nuevo-planeta-D_NQ_NP_952964-MLA41734512713_052020-V.webp" title="Algo de Lyna"></Book>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   </div>);
 }
