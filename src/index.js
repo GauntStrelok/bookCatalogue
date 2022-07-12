689034d83e1dbf2e01dc843d25067f3ee7ccd4e4
ezequielmgonzalez <e.gonzalez95@hotmail.com>
2022-07-11 21:08:01 -0300

    Modified index to have App component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
-import AppAux from "./AppAux";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppAux />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
