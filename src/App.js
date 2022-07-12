f6b687d475b11b39f517d0bcf58fb3b99b23c5cf
ezequielmgonzalez <e.gonzalez95@hotmail.com>
2022-07-12 13:32:25 -0300

    Removed unneccesary comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-// import Header from "./components/header";
-// import Footer from "./components/footer/Footer";
 import Router from "./pages/Router";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />   */}
       <Router />
-      {/* <Footer /> */}
     </div>
   );
 }
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-// import { useSelector } from "react-redux";
 import Home from "./Home";
 import Admin from "./Admin";
 import Delete from "./Delete";
 
 export default function Router() {
   const location = useLocation();
-  //   const userData = useSelector((state) => state.USER_LOGIN);
 
   return (
     <Routes location={location} key={location.pathname}>
